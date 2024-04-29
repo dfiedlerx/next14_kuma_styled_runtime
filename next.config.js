42441fe83bf2fa3ec73e392903db4451039912d0
Daniel Fiedler <daniel.fiedler@grupoboticario.com.br>
2024-04-29 11:45:49 -0300

    feat: add turbo next configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const { withKumaUI } = require("@kuma-ui/next-plugin");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 module.exports = withKumaUI(nextConfig);
